6edc45b89dc1a4cb4d1154f9fdafa30065cbbbaa
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-02 00:03:44 -0500

    More Changes Tomorrow

--- JS/site.js
+++ JS/site.js
@@ -1,3 +1,10 @@
-function helloworld() {
-    alert('Hello Javascript');
-}
+// write a function to get the elements by id 
+
+// parse the inputs into strings
+
+
+// write a function for a for loop from the start Char to the End Char
+
+//  write validation for inaccurate chars !# etc.
+
+//
