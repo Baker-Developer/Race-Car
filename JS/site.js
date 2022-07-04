cfd479ef66c144e0e8605ad16f34160e08af9cf3
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-03 20:33:40 -0500

    More Fixes Coming

--- JS/site.js
+++ JS/site.js
@@ -1,10 +1,26 @@
 // write a function to get the elements by id 
+function getElementsById() {
+    var stringInput = document.getElementById('stringInput');
+    var btnSubmit = document.getElementById('btnSubmit');
+    var alert = document.getElementById('alert');
+}
 
-// parse the inputs into strings
 
 
-// write a function for a for loop from the start Char to the End Char
+function reverseString() {
+  
+    // write a function for a for loop from the start Char to the End Char that will reverse the the chars
 
-//  write validation for inaccurate chars !# etc.
+    //  write validation for inaccurate chars !# etc.
 
-//
+    // write validation for a matching chars backwards
+}
+
+
+
+// write a function to display a string 
+function displayInput() {
+  // write code to that reveals the alert id from invisible to visible
+
+
+}
